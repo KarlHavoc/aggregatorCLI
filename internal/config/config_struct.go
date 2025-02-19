34be80614f632c5522dc9c08e6fa6e04d639d2af
carlhavoc <mkowalsky87@gmail.com>
2025-02-19 13:54:49 -0700

    initial commit

---
+++ internal/config/config_struct.go
@@ -0,0 +1,6 @@
+package internal
+
+type Config struct {
+	db_url text 'json:"url"'
+	current_user_name 'json:"current_user_name"'
+}
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/KarlHavoc/aggregatorCLI/internal"
+)
+
+func main() {
+
+}
