4edcfd423c336cd765d58d044f7656596f8367c3
carlhavoc <mkowalsky87@gmail.com>
2025-02-24 19:15:19 -0700

    extensions change

--- sql/schema/003_feed_follows.sql
+++ sql/schema/003_feed_follows.sql
@@ -3,10 +3,9 @@ CREATE TABLE feed_follows (
     id uuid PRIMARY KEY,
     created_at TIMESTAMP NOT NULL,
     updated_at TIMESTAMP NOT NULL,
-    user_id uuid NOT NULL references users(id) ON DELETE CASCADE,
-    feed_id uuid NOT NULL references feeds(id) ON DELETE CASCADE,
+    user_id uuid NOT NULL references users (id) ON DELETE CASCADE,
+    feed_id uuid NOT NULL references feeds (id) ON DELETE CASCADE,
     CONSTRAINT uq_user_id_feed_id UNIQUE (user_id, feed_id)
 );
-
 -- +goose Down
 DROP TABLE feed_follows;
